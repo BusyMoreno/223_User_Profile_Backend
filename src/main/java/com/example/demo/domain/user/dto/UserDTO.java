88984b853ab0294913fc752252295d26a432c8cd
Lisa Meergans <lisa.meergans@edu.tbz.ch>
2026-01-14 14:44:52 +0100

    add new fields to user
    add address, birth date and profile picture to user entity

--- src/main/java/com/example/demo/domain/user/dto/UserDTO.java
+++ src/main/java/com/example/demo/domain/user/dto/UserDTO.java
@@ -2,10 +2,12 @@ package com.example.demo.domain.user.dto;
 
 import com.example.demo.core.generic.AbstractDTO;
 import com.example.demo.domain.role.dto.RoleDTO;
+
+import java.time.LocalDate;
 import java.util.Set;
 import java.util.UUID;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -27,12 +29,29 @@ public class UserDTO extends AbstractDTO {
   @Valid
   private Set<RoleDTO> roles;
 
-  public UserDTO(UUID id, String firstName, String lastName, String email, Set<RoleDTO> roles) {
+  @NotBlank(message = "Address is mandatory")
+  private String address;
+
+  @NotNull(message = "Birth date is mandatory")
+  @Past(message = "Birth date must be in the past")
+  private LocalDate birthDate;
+
+  @NotBlank(message = "Profile picture is mandatory")
+  @Pattern(
+          regexp = "https?://.*",
+          message = "Invalid URL"
+  )
+  private String profilePicture;
+
+  public UserDTO(UUID id, String firstName, String lastName, String email, Set<RoleDTO> roles, String address, LocalDate birthDate, String profilePicture) {
     super(id);
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
     this.roles = roles;
+    this.address = address;
+    this.birthDate = birthDate;
+    this.profilePicture = profilePicture;
   }
 
 }
